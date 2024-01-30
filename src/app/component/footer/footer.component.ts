1802cdce345e732a17df7f6c41d50ac4ed7c1c0f
johanna <jmbanegas@sudamericano.edu.ec>
2024-01-30 01:31:40 -0500

    CORRECION

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -3,10 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    // Aquí agregarías otros módulos o componentes si los necesitas
+  ],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css'] // Corregido a styleUrls y en formato de array
 })
 export class FooterComponent {
-
+  // Aquí puedes agregar propiedades y métodos para tu componente
 }
